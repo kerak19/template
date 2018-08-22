3d0b4b2461b5c044048ca94449da6ab2d0b28707
Łukasz Opasiak <kerak19@hotmail.com>
2018-08-22 11:34:47 +0200

    Delete default from deleted_at

--- migrations/1533481189_create_users.up.sql
+++ migrations/1533481189_create_users.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE users(
     pass        bytea NOT NULL,
     created_at  timestamp DEFAULT(now() at time zone 'utc') NOT NULL,
     updated_at  timestamp DEFAULT(now() at time zone 'utc') NOT NULL,
-    deleted_at  timestamp DEFAULT(now() at time zone 'utc'),
+    deleted_at  timestamp,
     status      text NOT NULL default 'active', -- it should be good idea to make this some enum type
     role        text NOT NULL default 'user', -- this too
 
